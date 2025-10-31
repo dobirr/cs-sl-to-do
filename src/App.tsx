d23abe5790d780b5a0363812675c90e15b92dfd5
Dennis Dobirr <dennis.dobirr@realcore.de>
2025-10-31 15:24:39 +0100

    feat(ui): add Welcome component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.css";
-import Login from "./components/Login/Login.tsx";
+import './App.css';
+import Login from './components/Login/Login.tsx';
+import Welcome from './components/Welcome/Welcome.tsx';
 
 function App() {
   return (
     <div className="App">
+      <Welcome user={{ email: 'test@test.de' }} />
       <Login />
     </div>
   );
---
+++ src/components/Welcome/Welcome.tsx
@@ -0,0 +1,32 @@
+import { type FC } from 'react';
+import type { User } from '../../types';
+
+interface WelcomeProps {
+  user: User;
+}
+
+const Welcome: FC<WelcomeProps> = ({ user }) => {
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-header d-flex justify-content-between align-content-center">
+              <h4>Welcome to Todo App</h4>
+            </div>
+            <div className="card-body">
+              <h5>Hello, {user.email}</h5>
+              <p className="text-muted">You have successfully logged in.</p>
+              <p className="alert alert-info">
+                <strong>Coming Soon: </strong>Todo functionality will be added
+                here.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
